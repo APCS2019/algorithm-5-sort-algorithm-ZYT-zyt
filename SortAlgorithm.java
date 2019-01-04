184a1fe09c6afa421dfa3b0915c77742923d2de0
TImZhou <45361581+ZYT-zyt@users.noreply.github.com>
2019-01-04 14:37:46 +0800

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -1,38 +1,41 @@
 public class SortAlgorithm{
-  public static void mergeSort(int[] a, int start, int end){
-        if(end - start == 1){
-            return
+    public static void mergeSort(int[] a, int start, int end) {
+        if (end - start == 0) {
+            return;
         }
-        int mid = (int)((start + end)/2);
-        mergeSort(a,start,mid - 1);
-        mergeSort(a,mid,end);
-        merge(a,start,end);
-    }
-    public static void mergeSort(int[] a){
-        mergeSort(a,0,a.length-1);
-    }
-    public static void merge(int[] a, int start, int end){
-        int[] b = new int[end - start + 1];
-        int i = start;
-        int j = mid;
-        int index = 0;
-        while(i < mid ||j <= end){
-            if(i >= mid){
-                b[index++] = a[j++];
-            }
-            else if(j > end){
-                b[index++] = a[i++];
-            }
-            else if(a[i] < a[j]){
-                b[index++] = a[i++];
-            }
-            else{
-                b[index++] = a[j++];
-            }
-        }
-        for(int i : b){
-            a[start++] = i;
-        }
+        int left = (int) ((start + end) / 2);
+        int right = left + 1;
+        mergeSort(a, start, left);
+        mergeSort(a, right, end);
+        merge(a, start, end);
     }
 
+    public static void mergeSort(int[] a) {
+        mergeSort(a, 0, a.length - 1);
+    }
+
+    public static void merge(int[] a, int start, int end) {
+        if (end - start == 0)
+            return;
+        int[] b = new int[end - start + 1];
+        int i = start;
+        int left = ((int) (start + end) / 2);
+        int right = left + 1;
+        int j = right;
+        int index = 0;
+        while (i <= left || j <= end) {
+            if (i > left) {
+                b[index++] = a[j++];
+            } else if (j > end) {
+                b[index++] = a[i++];
+            } else if (a[i] < a[j]) {
+                b[index++] = a[i++];
+            } else {
+                b[index++] = a[j++];
+            }
+        }
+        for (int e : b) {
+            a[start++] = e;
+        }
+    }
 }
